8857af5239d38d499a5b98bce7935403c94fcac8
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2024-01-30 15:02:30 +0100

    Aggiungere i file di progetto.

---
+++ esercizioPome/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizioPome
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+        public Persona(string nome, string cognome, int eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+        // metodo
+        public string getDettagli() { return "Nome: " + Nome + "\t" + "Cognome: " + Cognome + "\t" + "Età: " + Eta; }
+    }
+}
---
+++ esercizioPome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizioPome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci nome: \t");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Inserisci cognome: \t");
+            string cognome = Console.ReadLine();
+            Console.WriteLine("Inserisci età: \t");
+            int eta = int.Parse(Console.ReadLine());
+            // Parse = metodo che converte una stringa ad un numero 
+
+            Persona Alice = new Persona(nome, cognome, eta);
+            Console.WriteLine(Alice.getDettagli());
+
+            Console.ReadKey();
+
+        }
+    }
+}
